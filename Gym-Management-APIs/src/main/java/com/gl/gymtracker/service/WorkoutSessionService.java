43337048524bdc9c5ee0bdaa04d76a6da8f863d8
sakshi-mohinia <164994733+sakshi-mohinia@users.noreply.github.com>
2025-05-15 10:34:26 +0530

    Update WorkoutSessionService.java

--- Gym-Management-APIs/src/main/java/com/gl/gymtracker/service/WorkoutSessionService.java
+++ Gym-Management-APIs/src/main/java/com/gl/gymtracker/service/WorkoutSessionService.java
@@ -11,10 +11,10 @@ import com.gl.gymtracker.repository.WorkoutSessionRepository;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+
 public class WorkoutSessionService {
 
-    @Autowired
+
     private WorkoutSessionRepository repository;
 
     public WorkoutSession save(WorkoutSession session) {
@@ -22,18 +22,18 @@ public class WorkoutSessionService {
     }
 
     public List<WorkoutSession> findAll() {
-        return repository.findAll();
+      
     }
 
     public Optional<WorkoutSession> findById(Long id) {
-        return repository.findById(id);
+      
     }
 
     public void deleteById(Long id) {
-        repository.deleteById(id);
+       
     }
 
     public List<WorkoutSession> getTop3ByCaloriesBurned() {
-        return repository.findTop3ByOrderByCaloriesBurnedDesc();
+       
     }
 }
